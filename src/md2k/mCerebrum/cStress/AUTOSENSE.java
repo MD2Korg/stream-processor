aea0ecf44604730b684753cc387b9d4c530b7b25
Timothy W. Hnat <twhnat@memphis.edu>
2015-07-09 13:34:11 -0500

    Initial commit of the java cStress code.

---
+++ src/md2k/mCerebrum/cStress/AUTOSENSE.java
@@ -0,0 +1,40 @@
+package md2k.mCerebrum.cStress;
+
+/**
+ * Created by hnat on 6/30/15.
+ */
+public class AUTOSENSE {
+
+    public static final int CHEST_ECG = 0;
+    public static final int CHEST_ACCEL_X = 1;
+    public static final int CHEST_ACCEL_Y = 2;
+    public static final int CHEST_ACCEL_Z = 3;
+    public static final int CHEST_GSR = 4;
+
+    public static final int CHEST_RIP = 7;
+    public static final int CHEST_BATTERY_SKINTEMP_AMBIENTTEMP = 8;
+
+    public static final int WRIST_ACCEL_X_RIGHT = 19;
+    public static final int WRIST_ACCEL_Y_RIGHT = 20;
+    public static final int WRIST_ACCEL_Z_RIGHT = 21;
+    public static final int WRIST_GYRO_X_RIGHT = 22;
+    public static final int WRIST_GYRO_Y_RIGHT = 23;
+    public static final int WRIST_GYRO_Z_RIGHT = 24;
+
+    public static final int WRIST_ACCEL_X_LEFT = 26;
+    public static final int WRIST_ACCEL_Y_LEFT = 27;
+    public static final int WRIST_ACCEL_Z_LEFT = 28;
+    public static final int WRIST_GYRO_X_LEFT = 29;
+    public static final int WRIST_GYRO_Y_LEFT = 30;
+    public static final int WRIST_GYRO_Z_LEFT = 31;
+
+
+    public static final int G_QUALITY_GOOD = 0;
+    public static final int G_QUALITY_MISSING = 4;
+    public static final int G_QUALITY_NOISE = 1;
+    public static final int G_QUALITY_BAND_LOOSE = 2;
+    public static final int G_QUALITY_BAND_OFF = 3;
+    public static final int G_QUALITY_BAD = 2;
+
+
+}
