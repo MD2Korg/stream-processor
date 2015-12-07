43e985b78ccfd368fcc9d9512dff1e2695f7ad7a
Timothy W. Hnat <twhnat@memphis.edu>
2015-12-07 16:56:18 -0600

    Fixed error in DataStream

--- src/main/java/md2k/mCerebrum/cStress/Library/DataStream.java
+++ src/main/java/md2k/mCerebrum/cStress/Library/DataStream.java
@@ -93,7 +93,7 @@ public class DataStream {
         }
     }
 
-    public void add(DataPoint dp) {
+    public void add(DataPoint dp) {f
         if (!Double.isNaN(dp.value) && !Double.isInfinite(dp.value)) {
             data.add(new DataPoint(dp));
             stats.addValue(dp.value);
