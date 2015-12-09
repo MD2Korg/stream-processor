c33a61bf51bb8abea0653436cbe696d904539d73
Timothy W. Hnat <twhnat@memphis.edu>
2015-12-09 13:23:33 -0600

    Integrate PuffMarker

--- src/main/java/md2k/mCerebrum/cStress/Autosense/PUFFMARKER.java
+++ src/main/java/md2k/mCerebrum/cStress/Autosense/PUFFMARKER.java
@@ -1,6 +1,8 @@
-package md2k.mCerebrum.cStress.Autosense;/*
+package md2k.mCerebrum.cStress.Autosense;
+
+/*
  * Copyright (c) 2015, The University of Memphis, MD2K Center 
- * - Timothy Hnat <twhnat@memphis.edu>
+ * - Nazir Saleneen <nsleheen@memphis.edu>
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
