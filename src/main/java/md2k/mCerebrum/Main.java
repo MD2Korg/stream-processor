40f21b33b4dafa3018c32e714021ed38bef36bfe
Timothy W. Hnat <twhnat@memphis.edu>
2016-03-08 12:04:11 -0600

    Reverted CSV file formatting

--- src/main/java/md2k/mCerebrum/Main.java
+++ src/main/java/md2k/mCerebrum/Main.java
@@ -42,7 +42,7 @@ public class Main {
 
         ExecutorService executor = Executors.newFixedThreadPool(4);
 
-        for (int i = 50; i < 51; i++) {
+        for (int i = 0; i < 23; i++) {
             String person = "SI" + String.format("%02d", i);
             Runnable worker = new WorkerThread(path + person,args[1]);
             executor.execute(worker);
