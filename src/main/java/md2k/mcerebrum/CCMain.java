3807eee79a60a371aa64da892e3940c782b622f1
Timothy W. Hnat <twhnat@memphis.edu>
2017-05-04 20:47:09 -0500

    Changed autosense activity threshold.

--- src/main/java/md2k/mcerebrum/CCMain.java
+++ src/main/java/md2k/mcerebrum/CCMain.java
@@ -29,7 +29,7 @@ import java.util.concurrent.Executors;
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-public class Main {
+public class CCMain {
 
     /**
      * Main driver class for replaying AutoSense data through StreamProcessor
--- src/main/java/md2k/mcerebrum/CSVParser.java
+++ src/main/java/md2k/mcerebrum/CSVParser.java
@@ -52,10 +52,10 @@ public class CSVParser implements Iterable<CSVDataPoint> {
         try {
             scanner = new Scanner(file);
             while (scanner.hasNext()) {
-                tokens = scanner.nextLine().split(" ");
-                double ts = Double.parseDouble(tokens[1]);
+                tokens = scanner.nextLine().split(",");
+                double ts = Double.parseDouble(tokens[0]);
                 timestamp = (long) ts;
-                data = Double.parseDouble(tokens[0]);
+                data = Double.parseDouble(tokens[2]);
 
                 tempPacket = new CSVDataPoint(channel, timestamp, data);
                 this.data.add(tempPacket);
